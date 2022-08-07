64c634ac6882972a12eb66fbbcbb861ef0d283f0
Suryaa <suryaacoolness@gmail.com>
2022-08-07 12:32:41 -0400

    Update addVM.py

--- addVM.py
+++ addVM.py
@@ -3,7 +3,7 @@ import subprocess
 import random
 import os
 
-from pyparsing import line
+#from pyparsing import line
 
 parser = argparse.ArgumentParser()
 
@@ -19,32 +19,25 @@ args = parser.parse_args()
 
 with open('portAvail.txt') as file:
     line1 = file.readline()
-    result = subprocess.run(['microstack.openstack flavor create', '--vcpu '+args.virtualcpus,'--disk '+args.storage,'--ram '+args.ram,'--property "pci_passthrough:alias"="'+args.gputype+':'+args.gpucount+'"',line1], stdout=subprocess.PIPE, shell=True)
-    os.system('microstack.openstack set'+line1+' --property "pci_passthrough:alias"="'+args.gputype+'Audio:'+args.gpucount+'"')
-    os.system('microstack launch'+args.os+' -flavor '+line1+' -n '+line1)
-    os.system('sudo sysctl -w net.ipv4.ip_forward=1')
+    print(line1)
+    os.system('microstack.openstack flavor create '+'--vcpu '+args.virtualcpus+' --disk '+args.storage+' --ram '+args.ram+' --property "pci_passthrough:alias"="'+args.gputype+':'+args.gpucount+'" '+line1)
+    os.system('microstack.openstack flavor set '+line1+' --property "pci_passthrough:alias"="'+args.gputype+'Audio:'+args.gpucount+'"')
+    os.system('microstack launch '+args.operatingsys+' --flavor '+line1+' -n '+line1)
+   # os.system('sudo sysctl -w net.ipv4.ip_forward=1')
 
     os.system('microstack.openstack server list --name '+line1+' > outIP.temp')
-    with open("out.temp") as f:
+    with open("outIP.temp") as f:
         for i in f:
             if i!=0:
                 j = str(i).split(' | ')
                 if '10' in j[3]:
                     ip=i.split(', ')[1]
-                    os.system('sudo iptables -t nat -A PREROUTING -i '+args.device+' -p tcp --dport '+line1+' -j  DNAT --to-destination '+ip+':22')
+                   # os.system('sudo iptables -t nat -A PREROUTING -i '+args.device+' -p tcp --dport '+line1+' -j  DNAT --to-destination '+ip+':22')
     os.remove('outIP.temp')
-        
+
 
 
 with open('portAvail.txt', 'r') as fin:
     data = fin.read().splitlines(True)
 with open('portAvail.txt', 'w') as fout:
     fout.writelines(data[1:])
-
-
-    
-
-
-
-
-
