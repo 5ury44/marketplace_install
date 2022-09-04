d3cbab5534239a6cb69d3b24a4e42a9b15d3f994
Suryaa <suryaacoolness@gmail.com>
2022-09-04 12:34:03 -0400

    delete updates

--- delVM.py
+++ delVM.py
@@ -8,3 +8,6 @@ args = parser.parse_args()
 parser.add_argument("-n","--name", default="", help="Number of Virtual CPUs in machine")
 
 os.system('microstack.openstack server delete '+args.name)
+os.system('microstack.openstack flavor delete '+args.name)
+os.system('rm '+args.name+'.txt')
+#remove txt and flavor
